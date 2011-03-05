145e552d5553615297db0d84dfd4aa4635322ac6
Alexander C. Schrepfer <github@maddcow.us>
2011-03-05 13:36:51 -0800

    Update github.

--- scripts/getraceplaque.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-_url="http://www.zombiemud.org/race-plaque.php"
-_base=${1-$(dirname ${0})}
-_file="$(date +%F).txt"
-
-wget -qO - ${_url} | sed -n '/^|   1|/,$p' | grep '^|' > ${_base}/${_file}
