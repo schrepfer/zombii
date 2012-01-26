de1f2aad036a08c2bc3d8159ce3ea59b7ad019a0
Alexander C. Schrepfer <github@maddcow.us>
2012-01-25 23:39:03 -0800

    Update github.

--- scripts/getplaque.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-_url="http://zombiemud.org/top-plaque.php"
-_base=${1-$(dirname ${0})}
-_file="$(date +%F).txt"
-
-wget -qO - ${_url} | sed -n '/^  1\./,/^1001\./p' > ${_base}/${_file}
